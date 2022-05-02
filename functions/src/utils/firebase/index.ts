6ed0182d2bc86c6b048832568187434894c4a3cd
take-cantik <take.cantik17@gmail.com>
2022-05-02 14:39:29 +0900

    Update: firebase config

--- functions/src/utils/firebase/index.ts
+++ functions/src/utils/firebase/index.ts
@@ -1,15 +1,14 @@
 import admin from 'firebase-admin'
+import { firebaseAdminOptions } from './config'
 
 if (admin.apps.length === 0) {
-  admin.initializeApp()
+  admin.initializeApp(firebaseAdminOptions)
 }
 
 // Auth
-
 export const auth = admin.auth()
 
 // Firestore
-
 export const db = admin.firestore()
 
 export { admin }
--- functions/src/utils/secrets.ts
+++ functions/src/utils/secrets.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 
 // LINE
-
 export const LINE_MESSAGING_CHANNEL_ACCESS_TOKEN = process.env.LINE_MESSAGING_CHANNEL_ACCESS_TOKEN as string
 export const LINE_MESSAGING_CHANNEL_SECRET = process.env.LINE_MESSAGING_CHANNEL_SECRET as string
+
+// Firebase
+export const FIREBASE_DATABASE_URL = process.env.DATABASE_URL as string
