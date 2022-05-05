86dd60b0329314378b3f7885f6b69bb00ddebc68
take-cantik <take.cantik17@gmail.com>
2022-05-06 01:22:29 +0900

    Add: UserRepository

---
+++ functions/src/Infrastructure/RepositoryImpl/Firebase/UserRepository.ts
@@ -0,0 +1,34 @@
+import { User } from '~/Domains/Entities/User'
+import { UserRepositoryInterface } from '~/Domains/Reopsitories/UserRepository'
+import { db } from '~/utils/firebase'
+import { errorLogger } from '~/utils/util'
+
+export class UserRepository implements UserRepositoryInterface {
+  async getUser(lineId: string): Promise<User | null> {
+    try {
+      const res = await db.collection('users').doc(lineId).get()
+
+      if (res.data()) {
+        return {
+          lineId: res.data()!.lineId,
+          name: res.data()!.name,
+          rate: res.data()!.rate
+        }
+      }
+
+      return null
+    } catch (err) {
+      errorLogger(err)
+      throw new Error('getUser')
+    }
+  }
+
+  async addUser(user: User): Promise<void> {
+    try {
+      await db.collection('users').doc(user.lineId).set(user)
+    } catch (err) {
+      errorLogger(err)
+      throw new Error('addUser')
+    }
+  }
+}
