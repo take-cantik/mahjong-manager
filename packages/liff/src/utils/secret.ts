4174c7d2a3f07151155c32a1ad55d6c321f4c294
take-cantik <take.cantik17@gmail.com>
2022-08-21 11:23:55 +0900

    Add: Google env vars to liff

--- packages/liff/src/utils/secret.ts
+++ packages/liff/src/utils/secret.ts
@@ -1,1 +1,5 @@
 export const NEXT_PUBLIC_LIFF_ID = process.env.NEXT_PUBLIC_LIFF_ID as string
+
+export const GOOGLE_CLOUD_ID = process.env.GOOGLE_CLOUD_ID as string
+export const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL as string
+export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY as string
