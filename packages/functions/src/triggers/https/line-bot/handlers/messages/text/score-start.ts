d84ef99c30052338f3ea2a255db4fbec367d38e0
take-cantik <take.cantik17@gmail.com>
2022-08-26 18:28:41 +0900

    Fix: state change timing

--- packages/functions/src/triggers/https/line-bot/handlers/messages/text/score-start.ts
+++ packages/functions/src/triggers/https/line-bot/handlers/messages/text/score-start.ts
@@ -1,6 +1,7 @@
 import { MessageEvent } from '@line/bot-sdk'
 import { v4 as uuidv4 } from 'uuid'
 import { ResultRepository } from '~/Infrastructure/RepositoryImpl/Firebase/ResultRepository'
+import { StateRepository } from '~/Infrastructure/RepositoryImpl/Firebase/StateRepository'
 import { getCurrentTime } from '~/utils/day'
 import { lineClient } from '~/utils/line'
 import { msgSelectGame } from '~line/notice-messages/select-game'
@@ -12,8 +13,15 @@ interface Props {
 export const scoreStartHandler = async (props: Props) => {
   const { event } = props
 
-  const resultRepository = new ResultRepository()
-  const docId = await resultRepository.setTime(getCurrentTime())
-  const uuid = uuidv4()
-  await lineClient.replyMessage(event.replyToken, msgSelectGame(docId, uuid))
+  if (event.source.type === 'group') {
+    const resultRepository = new ResultRepository()
+    const stateRepository = new StateRepository()
+    const currentState = await stateRepository.getCurrentState(event.source.groupId)
+    if (currentState) {
+      await resultRepository.deleteResult(currentState.docId)
+    }
+    const docId = await resultRepository.setTime(getCurrentTime())
+    const uuid = uuidv4()
+    await lineClient.replyMessage(event.replyToken, msgSelectGame(docId, uuid))
+  }
 }
--- packages/functions/src/triggers/https/line-bot/handlers/postbacks/confirm.ts
+++ packages/functions/src/triggers/https/line-bot/handlers/postbacks/confirm.ts
@@ -1,6 +1,7 @@
 import { PostbackEvent } from '@line/bot-sdk'
 import { User } from '~/Domains/Entities/User'
 import { ResultRepository } from '~/Infrastructure/RepositoryImpl/Firebase/ResultRepository'
+import { StateRepository } from '~/Infrastructure/RepositoryImpl/Firebase/StateRepository'
 import { UserRepository } from '~/Infrastructure/RepositoryImpl/Firebase/UserRepository'
 import { lineClient } from '~/utils/line'
 import { getData, getDocId } from '~/utils/postback'
@@ -21,6 +22,7 @@ export interface Participant extends User {
 export const confirmHandler = async (event: PostbackEvent): Promise<void> => {
   const resultRepository = new ResultRepository()
   const userRepository = new UserRepository()
+  const stateRepository = new StateRepository()
 
   const data = getData(event)
   const docId = getDocId(event)
@@ -60,94 +62,99 @@ export const confirmHandler = async (event: PostbackEvent): Promise<void> => {
 
     if (result.people === 4) {
       // 四麻の計算
-      participantList.forEach(async (participant) => {
-        const rateDiff = getRateDiff(participant, result, defaultScore, totalRate.four)
-        if (isNaN(rateDiff)) {
-          await resultRepository.setScore(docId, [])
-          await lineClient.replyMessage(event.replyToken, {
-            type: 'text',
-            text: '計算でエラーが発生しました。もう一度得点を入力してください'
+      await Promise.all(
+        participantList.map(async (participant) => {
+          const rateDiff = getRateDiff(participant, result, defaultScore, totalRate.four)
+          if (isNaN(rateDiff)) {
+            await resultRepository.setScore(docId, [])
+            await lineClient.replyMessage(event.replyToken, {
+              type: 'text',
+              text: '計算でエラーが発生しました。もう一度得点を入力してください'
+            })
+            return
+          }
+
+          participant.fourRecord.rate += rateDiff
+          participant.fourRecord.gameCount++
+          participant.fourRecord.rankCount += participant.order
+          participant.fourRecord.rankHistory.push(participant.order)
+          participant.fourRecord.rankHistory = participant.fourRecord.rankHistory.slice(-10)
+          if (participant.order === 1) {
+            participant.fourRecord.firstCount++
+          } else if (participant.order === 2) {
+            participant.fourRecord.secondCount++
+          } else if (participant.order === 3) {
+            participant.fourRecord.thirdCount++
+          } else if (participant.order === 4) {
+            participant.fourRecord.fourthCount++
+          }
+          if (participant.score < 0) {
+            participant.fourRecord.minusCount++
+          }
+
+          rateResultList.push({
+            userName: participant.name,
+            newRate: participant.fourRecord.rate,
+            rateDiff
           })
-          return
-        }
 
-        participant.fourRecord.rate += rateDiff
-        participant.fourRecord.gameCount++
-        participant.fourRecord.rankCount += participant.order
-        participant.fourRecord.rankHistory.push(participant.order)
-        participant.fourRecord.rankHistory = participant.fourRecord.rankHistory.slice(-10)
-        if (participant.order === 1) {
-          participant.fourRecord.firstCount++
-        } else if (participant.order === 2) {
-          participant.fourRecord.secondCount++
-        } else if (participant.order === 3) {
-          participant.fourRecord.thirdCount++
-        } else if (participant.order === 4) {
-          participant.fourRecord.fourthCount++
-        }
-        if (participant.score < 0) {
-          participant.fourRecord.minusCount++
-        }
-
-        rateResultList.push({
-          userName: participant.name,
-          newRate: participant.fourRecord.rate,
-          rateDiff
+          await userRepository.updateRate({
+            lineId: participant.lineId,
+            name: participant.name,
+            threeRecord: participant.threeRecord,
+            fourRecord: participant.fourRecord
+          })
         })
-
-        await userRepository.updateRate({
-          lineId: participant.lineId,
-          name: participant.name,
-          threeRecord: participant.threeRecord,
-          fourRecord: participant.fourRecord
-        })
-      })
+      )
     } else {
-      participantList.forEach(async (participant) => {
-        const rateDiff = getRateDiff(participant, result, defaultScore, totalRate.three)
-        if (isNaN(rateDiff)) {
-          await resultRepository.setScore(docId, [])
-          await lineClient.replyMessage(event.replyToken, {
-            type: 'text',
-            text: '計算でエラーが発生しました。もう一度得点を入力してください'
+      await Promise.all(
+        participantList.map(async (participant) => {
+          const rateDiff = getRateDiff(participant, result, defaultScore, totalRate.three)
+          if (isNaN(rateDiff)) {
+            await resultRepository.setScore(docId, [])
+            await lineClient.replyMessage(event.replyToken, {
+              type: 'text',
+              text: '計算でエラーが発生しました。もう一度得点を入力してください'
+            })
+            return
+          }
+
+          participant.threeRecord.rate += rateDiff
+          participant.threeRecord.gameCount++
+          participant.threeRecord.rankCount += participant.order
+          participant.threeRecord.rankHistory.push(participant.order)
+          participant.threeRecord.rankHistory = participant.threeRecord.rankHistory.slice(-10)
+          if (participant.order === 1) {
+            participant.threeRecord.firstCount++
+          } else if (participant.order === 2) {
+            participant.threeRecord.secondCount++
+          } else if (participant.order === 3) {
+            participant.threeRecord.thirdCount++
+          }
+          if (participant.score < 0) {
+            participant.threeRecord.minusCount++
+          }
+
+          rateResultList.push({
+            userName: participant.name,
+            newRate: participant.threeRecord.rate,
+            rateDiff
           })
-          return
-        }
 
-        participant.threeRecord.rate += rateDiff
-        participant.threeRecord.gameCount++
-        participant.threeRecord.rankCount += participant.order
-        participant.threeRecord.rankHistory.push(participant.order)
-        participant.threeRecord.rankHistory = participant.threeRecord.rankHistory.slice(-10)
-        if (participant.order === 1) {
-          participant.threeRecord.firstCount++
-        } else if (participant.order === 2) {
-          participant.threeRecord.secondCount++
-        } else if (participant.order === 3) {
-          participant.threeRecord.thirdCount++
-        }
-        if (participant.score < 0) {
-          participant.threeRecord.minusCount++
-        }
-
-        rateResultList.push({
-          userName: participant.name,
-          newRate: participant.threeRecord.rate,
-          rateDiff
+          await userRepository.updateRate({
+            lineId: participant.lineId,
+            name: participant.name,
+            threeRecord: participant.threeRecord,
+            fourRecord: participant.fourRecord
+          })
         })
-
-        await userRepository.updateRate({
-          lineId: participant.lineId,
-          name: participant.name,
-          threeRecord: participant.threeRecord,
-          fourRecord: participant.fourRecord
-        })
-      })
+      )
     }
 
     await lineClient.replyMessage(event.replyToken, msgRateResult(rateResultList))
-  } else if (data === 'やり直す') {
+  } else if (data === 'やり直す' && event.source.type === 'group') {
     await resultRepository.setScore(docId, [])
+    await stateRepository.changeState({ groupId: event.source.groupId, docId })
     await lineClient.replyMessage(event.replyToken, { type: 'text', text: '得点を入力してください' })
   }
 }
