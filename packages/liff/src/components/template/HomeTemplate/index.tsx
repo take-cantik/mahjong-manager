2052ac2a12250e68aafee938740a14a24a2cf060
take-cantik <take.cantik17@gmail.com>
2022-10-01 18:59:39 +0900

    Add: ThreeResult and FourResult compoennt

--- packages/liff/src/components/template/HomeTemplate/index.tsx
+++ packages/liff/src/components/template/HomeTemplate/index.tsx
@@ -1,56 +1,20 @@
-import { useContext } from 'react'
+import { useRecoilValue } from 'recoil'
 
+import { FourResult } from '~/components/domain/home/FourResult'
+import { ThreeResult } from '~/components/domain/home/ThreeResult'
 import { Header } from '~/components/shared/Header'
-import type { InformationProps } from '~/components/shared/Information'
-import { Profile } from '~/components/shared/Profile'
-import { ResultDetail } from '~/components/shared/ResultDetail'
-import { ResultTransition } from '~/components/shared/ResultTransition'
 import { TabMenu } from '~/components/shared/TabMenu'
-import { AuthContext } from '~/contexts/AuthContext'
+import { gameState } from '~/state/game'
 
 import * as styles from './styles'
 
 export const HomeTemplate = () => {
-  const { user } = useContext(AuthContext)
-  const testData: InformationProps[] = [
-    {
-      label: '対戦数',
-      content: '45'
-    },
-    {
-      label: '平均順位',
-      content: '2.5'
-    },
-    {
-      label: '1位率',
-      content: '25%'
-    },
-    {
-      label: '2位率',
-      content: '25%'
-    },
-    {
-      label: '3位率',
-      content: '25%'
-    },
-    {
-      label: '4位率',
-      content: '25%'
-    },
-    {
-      label: '飛び率',
-      content: '1.55%'
-    }
-  ]
+  const game = useRecoilValue(gameState)
 
   return (
     <>
       <Header />
-      <main css={styles.main}>
-        <Profile name={user!.name} rate={user!.threeRecord.rate} />
-        <ResultTransition rankList={[1, 2, 1, 3, 3, 2, 4, 2]} />
-        <ResultDetail recordList={testData} />
-      </main>
+      <main css={styles.main}>{game.people === 4 ? <FourResult /> : <ThreeResult />}</main>
       <TabMenu />
     </>
   )
