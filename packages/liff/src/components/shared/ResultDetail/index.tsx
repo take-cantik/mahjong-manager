46984577ea401a332004945a2b1660b37da921ef
take-cantik <take.cantik17@gmail.com>
2022-10-01 14:22:38 +0900

    Update: style

--- packages/liff/src/components/shared/ResultDetail/index.tsx
+++ packages/liff/src/components/shared/ResultDetail/index.tsx
@@ -3,15 +3,17 @@ import type { ComponentPropsWithRef } from 'react'
 import type { InformationProps } from '~/components/shared/Information'
 import { Information } from '~/components/shared/Information'
 
+import * as styles from './styles'
+
 interface ResultDetailProps extends ComponentPropsWithRef<'section'> {
   recordList: InformationProps[]
 }
 
 export const ResultDetail = ({ recordList }: ResultDetailProps): JSX.Element => {
   return (
-    <section>
-      <h2>対戦詳細</h2>
-      <div>
+    <section css={styles.common}>
+      <h2 css={styles.title}>対戦詳細</h2>
+      <div css={styles.wrapper}>
         {recordList.map((record) => (
           <Information label={record.label} content={record.content} key={record.label} />
         ))}
---
+++ packages/liff/src/components/shared/ResultDetail/styles.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react'
+
+export const common = css`
+  width: 100%;
+  padding-bottom: 50px;
+`
+
+export const title = css`
+  font-size: 2rem;
+  font-weight: bold;
+`
+export const wrapper = css`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px 0;
+  padding: 12px 16px 0;
+`
