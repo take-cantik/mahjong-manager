ccedcb511f111e77e741af214dbacab791210785
take-cantik <take.cantik17@gmail.com>
2022-10-17 20:16:33 +0900

    Fix: resion

--- packages/liff/src/infra/firebase/index.ts
+++ packages/liff/src/infra/firebase/index.ts
@@ -4,6 +4,7 @@ import type { Auth } from 'firebase/auth'
 import { getAuth } from 'firebase/auth'
 import type { Firestore } from 'firebase/firestore'
 import { getFirestore } from 'firebase/firestore'
+import type { Functions } from 'firebase/functions'
 import { getFunctions } from 'firebase/functions'
 
 import { firebaseConfig } from './config'
@@ -11,4 +12,4 @@ import { firebaseConfig } from './config'
 export const firebase: FirebaseApp = initializeApp(firebaseConfig)
 export const firestore: Firestore = getFirestore(firebase)
 export const auth: Auth = getAuth(firebase)
-export const functions = getFunctions(firebase)
+export const functions: Functions = getFunctions(firebase, 'asia-northeast1')
