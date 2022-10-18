ecfda78614e5465670115a03360eef068aa51eba
take-cantik <take.cantik17@gmail.com>
2022-10-18 13:35:04 +0900

    Update: meta

--- packages/liff/src/pages/_document.tsx
+++ packages/liff/src/pages/_document.tsx
@@ -4,7 +4,7 @@ const MyDocument = () => {
   return (
     <Html lang="ja-JP" className="dark">
       <Head>
-        <meta name="application-name" content="LiFF + Nextjs + MUI テンプレート" />
+        <meta name="じゃんま！" content="麻雀レート管理アプリ" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Noto+Sans+JP:wght@400;500;700&family=Noto+Serif+JP:wght@400;500;700&family=BIZ+UDPGothic:wght@400;500;700&display=swap"
           rel="stylesheet"
