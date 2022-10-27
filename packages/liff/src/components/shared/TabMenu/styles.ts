2a13edbba54d004e067fad6367203b06d4f8a8a0
take-cantik <take.cantik17@gmail.com>
2022-10-27 18:14:04 +0900

    Add: after

--- packages/liff/src/components/shared/TabMenu/styles.ts
+++ packages/liff/src/components/shared/TabMenu/styles.ts
@@ -1,8 +1,19 @@
 import { css } from '@emotion/react'
 
+import { colors } from '~/styles/themes'
+
 export const common = css`
   width: 100%;
   display: flex;
   position: absolute;
   bottom: 0;
+
+  &::after {
+    content: '';
+    width: 100%;
+    height: 24px;
+    position: absolute;
+    bottom: -24px;
+    background: ${colors.black.lighten[5]};
+  }
 `
